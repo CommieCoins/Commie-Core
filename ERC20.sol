a512c8ab741e69c11aa2345424c79e76e86c64fc
Commie Coin <36435124+CommieCoins@users.noreply.github.com>
2018-02-23 20:24:00 -0800

    Initial Commit
    Uploaded initial test version of the Commie Coin ERC20 Contract. Removing coins from the wallets is active. Redistributing the coins to wallet is Under Development.

---
+++ ERC20.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.18;
+
+// ----------------------------------------------------------------------------
+// 'CCCP' 'Commie Coin' token contract
+//
+// Symbol      : CCCP
+// Name        : Commie Coin
+// Total supply: Generated from contributions
+// Decimals    : 18
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function deductaccounts(address from) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+
+// ----------------------------------------------------------------------------
+// Contract function to receive approval and execute function in one call
+// ----------------------------------------------------------------------------
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
